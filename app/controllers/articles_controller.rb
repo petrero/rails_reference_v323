93f919d3e7cf0742c12aaac68d61490d5b305258
Petre Diaconescu <petrero@gmail.com>
2012-05-20 17:02:41 +0300

    if is provided the show article url before updating article request is redirected to the show article path after updating article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,9 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-    
+    if request.path != article_path(@article)
+      redirect_to @article, status: :moved_permanently
+    end
   end
 
   def new
