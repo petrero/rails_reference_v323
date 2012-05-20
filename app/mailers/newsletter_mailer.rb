f5621cf44f8c755683e6d9280676419c467322f2
Petre Diaconescu <petrero@gmail.com>
2012-05-20 10:10:19 +0300

    creating newsletter mailer and associated views

---
+++ app/mailers/newsletter_mailer.rb
@@ -0,0 +1,13 @@
+class NewsletterMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.newsletter_mailer.weekly.subject
+  #
+  def weekly(email)
+   
+    mail to: email, subject: "Railscasts weekly"
+  end
+end
