b5d896cd4ec02caf08a313a8e6bdffbde7c41500
Petre Diaconescu <petrero@gmail.com>
2012-05-20 12:13:57 +0300

    moving logic from mailman_server to ticket class

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,12 @@
 class Ticket < ActiveRecord::Base
   attr_accessible :body, :from, :subject
+  
+  def self.receive_mail(message)
+    ticket_id = message.subject[/^Update (\d+)$/, 1]
+    if ticket_id.present?
+      Ticket.update(ticket_id, body: message.body.decoded)
+    else
+      Ticket.create! subject: message.subject, body: message.body.decoded, from: message.from.first
+    end
+  end
 end
