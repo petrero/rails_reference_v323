b33ed6197dceb98c2f0440309889dfa52fdfc848
Petre Diaconescu <petrero@gmail.com>
2012-05-20 16:58:05 +0300

    show article url before and after updating article are now both usable

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,9 +2,9 @@ class Article < ActiveRecord::Base
   attr_accessible :content, :name, :published_at
   
   extend FriendlyId
-  friendly_id :name, use: :slugged
+  friendly_id :name, use: [:slugged, :history]
   
-  def should_generate_new_friendly_id?
-    new_record?
-  end
+  #def should_generate_new_friendly_id?
+  #  new_record?
+  #end
 end
