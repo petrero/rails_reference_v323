2d27a31039a1156eefa16693295942fb792194ff
Petre Diaconescu <petrero@gmail.com>
2012-05-20 10:48:59 +0300

    usind roadie gem for viewing html emails correctly in email clients, here i using gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,17 @@
 RailsReferenceV323::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.action_mailer.default_url_options = {host: "railscasts.com"}
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    doamin: "railscasts.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -14,7 +25,7 @@ RailsReferenceV323::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
