e32f60daf2f62e905c0b26b0e02467e993e5fc42
Petre Diaconescu <petrero@gmail.com>
2012-05-20 16:37:12 +0300

    adding name in show article url with friendly_id gem

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,6 @@
 class Article < ActiveRecord::Base
   attr_accessible :content, :name, :published_at
   
-  def to_param
-    "#{id} #{name}".parameterize
-  end
+  extend FriendlyId
+  friendly_id :name
 end
