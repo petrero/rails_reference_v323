4a5d58754b7d4a649c93204c26b98a752c4be4f9
Petre Diaconescu <petrero@gmail.com>
2012-05-27 12:38:06 +0300

    using ActiveAttr for the message model that is not backed up  by a rable in a database

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,21 +1,11 @@
 class Message
-  include ActiveModel::Validations
-  include ActiveModel::Conversion
-  extend ActiveModel::Naming
-
-  attr_accessor :name, :email, :content
-
+  include ActiveAttr::Model
+  
+  attribute :name
+  attribute :email
+  attribute :content
+  
   validates_presence_of :name
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
   validates_length_of :content, :maximum => 500
-
-  def initialize(attributes = {})
-    attributes.each do |name, value|
-      send("#{name}=", value)
-    end
-  end
-
-  def persisted?
-    false
-  end
 end
