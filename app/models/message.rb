412fa213aa79e7255d31faa50652c10e51aacdeb
Petre Diaconescu <petrero@gmail.com>
2012-05-27 12:44:04 +0300

    adding mass assignment protection to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,9 @@ class Message
   attribute :name
   attribute :email
   attribute :content
+  attribute :priority, type: Integer, default: 0
+  
+  attr_accessible :name, :email, :content
   
   validates_presence_of :name
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
