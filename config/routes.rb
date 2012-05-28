62e83aef06362218e7110f85a2e8ca5099a87f06
Petre Diaconescu <petrero@gmail.com>
2012-05-28 21:32:08 +0300

    pretyfing new user url and sessions url

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RailsReferenceV323::Application.routes.draw do
+  get 'signup', to: "users#new", as: 'signup'
+  get 'login', to: "sessions#new", as: 'login'
+  get 'logout', to: "sessions#destroy", as: 'logout'
+
   root to: 'articles#index'
   resources :articles
   
