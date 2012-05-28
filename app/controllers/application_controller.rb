b37c2f6286d53c9020ea5fe3776e9b2dd13e3e71
Petre Diaconescu <petrero@gmail.com>
2012-05-28 21:50:44 +0300

    user must log in before edit an article

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,9 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
+  def authorize
+    redirect_to login_url, alert: "Not authorized" if current_user.nil?
+  end
+  
   helper_method :current_user
 end
