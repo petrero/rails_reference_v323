8dd02ffc8556ee8ea745d3744a2d4c391035285b
Petre Diaconescu <petrero@gmail.com>
2012-05-28 22:19:22 +0300

    adding remember me password functionality in login form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password_confirmation, :password
   has_secure_password
+  before_create { generate_token(:auth_token) }
   
   validates_uniqueness_of :email
+  
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120528172904) do
+ActiveRecord::Schema.define(:version => 20120528185445) do
 
   create_table "articles", :force => true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20120528172904) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "auth_token"
   end
 
 end
