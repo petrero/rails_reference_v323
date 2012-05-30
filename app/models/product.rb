cf794857d56713b094abb51506183d562b45c599
Petre Diaconescu <petrero@gmail.com>
2012-05-30 17:49:58 +0300

    adding validation messages

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
-  attr_accessible :discontinued, :name, :price, :rating, :released_on, :publisher, :categories
+  attr_accessible :publisher_id, :category_ids, :discontinued, :name, :price, :rating, :released_on, :publisher, :categories
   belongs_to :publisher
   has_many :categorizations
   has_many :categories, through: :categorizations
+  
+  validates_presence_of :name
 end
