aeb7aa97dae70c33588a1785eb0e9639aa7b8d7e
Petre Diaconescu <petrero@gmail.com>
2012-06-01 21:37:42 +0300

    the experiment will persist after it's finished so the user will see the same variation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      finished("signup_title")
+      finished("signup_title", reset: false)
       redirect_to root_url, notice: "Thank you for signing up."
     else
       render "new"
